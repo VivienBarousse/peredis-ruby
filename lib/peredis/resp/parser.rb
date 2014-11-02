4346bf4601966b6bbb84d3a2201cb70008040a18
Vivien Barousse <barousse.vivien@gmail.com>
2014-11-02 14:36:22 +0000

    Add support for parsing bulk strings.

--- lib/peredis/resp/parser.rb
+++ lib/peredis/resp/parser.rb
@@ -10,6 +10,8 @@ module Peredis
         case input.read(1)
         when '+'
           next_string
+        when '$'
+          next_bulk_string
         end
       end
 
@@ -17,6 +19,22 @@ module Peredis
 
       def next_string
         input.readline.strip
+      end
+
+      def next_bulk_string
+        length = Integer(input.readline.strip)
+
+        # Support for null bulk strings
+        if length < 0
+          return nil
+        end
+
+        string = input.read(length)
+
+        # Discard the line break after the string
+        input.readline
+
+        string
       end
 
       attr_reader :input
--- spec/peredis/resp/parser_spec.rb
+++ spec/peredis/resp/parser_spec.rb
@@ -22,6 +22,51 @@ describe Peredis::Resp::Parser do
     end
   end
 
+  describe "bulk strings" do
+    let(:data) { "$6\r\nfoobar\r\n" }
+    
+    it "should return the string object" do
+      expect(subject.next).to eq("foobar")
+    end
+
+    it "should consume the whole buffer" do
+      subject.next
+      expect(input.read).to eq("")
+    end
+
+    context "when the string contains a CRLF character" do
+      let(:data) { "$8\r\nfoo\r\nbar\r\n" }
+
+      it "should return the string with the CRLF character" do
+        expect(subject.next).to eq("foo\r\nbar")
+      end
+
+      context "when the string contains a CRLF character at the end" do
+        let(:data) { "$8\r\nfoobar\r\n\r\n" }
+
+        it "should return the string with the CRLF character" do
+          expect(subject.next).to eq("foobar\r\n")
+        end
+      end
+    end
+
+    describe "empty bulk string" do
+      let(:data) { "$0\r\n\r\n" }
+
+      it "should return the empty string" do
+        expect(subject.next).to eq("")
+      end
+    end
+
+    describe "null bulk string" do
+      let(:data) { "$-1\r\n" }
+
+      it "should return nil" do
+        expect(subject.next).to eq(nil)
+      end
+    end
+  end
+
   describe "multiple objects" do
     describe "multiple strings" do
       let(:data) { "+abc\r\n+def\r\n" }
@@ -29,6 +74,15 @@ describe Peredis::Resp::Parser do
       it "should return each string, in order" do
         expect(subject.next).to eq("abc")
         expect(subject.next).to eq("def")
+      end
+    end
+
+    describe "multiple bulk strings" do
+      let(:data) { "$3\r\nfoo\r\n$3\r\nbar\r\n" }
+
+      it "should return each string, in order" do
+        expect(subject.next).to eq("foo")
+        expect(subject.next).to eq("bar")
       end
     end
   end
